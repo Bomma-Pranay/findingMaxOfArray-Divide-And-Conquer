dd11f20198324818e31f9569e80570935b20aed5
Bomma-Pranay <62324691+Bomma-Pranay@users.noreply.github.com>
2020-03-20 13:41:12 +0530

    Update SourceCode.py

--- SourceCode.py
+++ SourceCode.py
@@ -5,6 +5,6 @@ def f(a,lo,hi):
     m=max(f(a,lo,mid),f(a,mid+1,hi))
     return m
 #Give elements as input
-a=list(map(int,input().split())
+a=list(map(int,input().split()))
 mx=f(a,0,len(a)-1)
 print('maximum element in',a,'is ',mx)
